9b6901d30ac913939fe18145f8da470a9367b6c3
AliiiBenn <davidddpereiraaa6@gmail.com>
2025-11-26 08:27:12 +0100

    Added fieldType and field functions

--- packages/collections/src/fields/field.ts
+++ packages/collections/src/fields/field.ts
@@ -1,3 +1,40 @@
+import z from "zod";
+import {
+  Field,
+  FieldConfig,
+  FieldPermissions,
+  FieldTypeConfig,
+  FieldTypeFinal,
+} from "./types";
 
+export const fieldType =
+  <TParams extends z.ZodType>(config: FieldTypeConfig<TParams>) =>
+  (params: z.infer<TParams>): FieldTypeFinal<TParams> => {
+    const validated = config.schema.parse(params);
 
-export const field = () => {}
+    return {
+      kind: config.dsl.kind,
+      params: validated,
+      dsl: config.dsl,
+      admin: config.admin,
+    };
+  };
+
+const defaultPermissions: FieldPermissions = {
+  create: async () => true,
+  read: async () => true,
+  update: async () => true,
+  delete: async () => true,
+};
+
+export const field = <TType extends FieldTypeConfig>(
+  config: FieldConfig<TType>,
+): Field<TType> => {
+  return {
+    type: config.type,
+    permissions: {
+      ...defaultPermissions,
+      ...config.permissions,
+    },
+  };
+};
